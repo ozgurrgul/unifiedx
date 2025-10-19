f2b1e9e152d9d27934b18047d1e9167cb81716f8
ozgur <lugrugzo@gmail.com>
2025-10-19 09:20:26 +0300

    chore: theme provider moved to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter, IBM_Plex_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const font1 = Inter({
   weight: "400",
@@ -17,9 +18,11 @@ const font2 = IBM_Plex_Mono({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${font1.variable} ${font2.variable}`}>
-      <Component {...pageProps} />
-      <Analytics />
-    </main>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <main className={`${font1.variable} ${font2.variable}`}>
+        <Component {...pageProps} />
+        <Analytics />
+      </main>
+    </ThemeProvider>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,3 @@
-import { ThemeProvider } from "@/components/theme-provider";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -6,10 +5,8 @@ export default function Document() {
     <Html lang="en" suppressHydrationWarning>
       <Head />
       <body>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-          <Main />
-          <NextScript />
-        </ThemeProvider>
+        <Main />
+        <NextScript />
       </body>
     </Html>
   );
